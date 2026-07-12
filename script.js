892ea53eeba8ca3895906bf4570c22c57c72a0e7
nikhiljanjal <nikhiljanjald@gmail.com>
2026-07-12 17:10:21 +0530

    Add alert for admission portal availability

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const btn = document.querySelector("button");
 
+  btn.addEventListener("click", function () {
+    alert("Admission Portal will be available soon!");
+  });
+});
