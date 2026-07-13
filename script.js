3effc01196ef7c4c836ec5208f7807ec0a6dae1c
nikhiljanjal <nikhiljanjald@gmail.com>
2026-07-13 16:25:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ document.addEventListener("DOMContentLoaded", function () {
     alert("Admission Portal will be available soon!");
   });
 });
+function openPopup(){
+    document.getElementById("popup").style.display = "block";
+}
+
+function closePopup(){
+    document.getElementById("popup").style.display = "none";
+}
+
+window.onclick = function(event){
+    const popup = document.getElementById("popup");
+    if(event.target == popup){
+        popup.style.display = "none";
+    }
+}
